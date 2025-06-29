39fbece7f1d5b03ae488d8affdba0f8ea93e9014
ayushudaydubey <102001ayushdubey@gmail.com>
2025-06-29 22:27:55 +0530

    app

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ dotenv.config();
 
 const app = express();
 
-
 app.use(cors({
   origin: ['http://localhost:5173', 'https://stripe-frontend-sooty.vercel.app'],
   credentials: true,
@@ -15,10 +14,11 @@ app.use(cors({
 
 app.use(express.json());
 
+app.get('/test', (req, res) => {
+  res.send('Stripe backend is running!');
+});
 
 app.use('/', productRoutes);
-
-
 
 app.listen(3000, () => {
   console.log(` Server is running `);
